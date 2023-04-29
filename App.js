8fdea516dfb3a5b9c9b72bf8afddbb1e649fdeb3
Prasanth Louis <prasanthlouis21@gmail.com>
2023-04-28 21:58:49 -0400

    Adding welcome screen

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Platform } from 'react-native';
+import { Text, View, Platform, Image, ImageBackground } from 'react-native';
 import { Button, NativeBaseProvider } from 'native-base';
 
-
+let myBackground = require('./assets/landing.jpg');
 export default function App() {
   return (
     <NativeBaseProvider>
     <View style={styles.container}>
-      <Text>Open up App.js tosds start working on Hello's app!</Text>
-      <Button><Text>Hello World</Text></Button>
-      <StatusBar style="auto" />
+<ImageBackground source={myBackground} style={{width: '100%', height: '100%'}}>
+  <View style={styles.viewStyle}>
+    <Text style={styles.titleStyle}> Welcome to PokeSearch</Text>
+    <Button
+    block={true}
+    style={styles.buttonStyle}
+    onPress={() => {}}
+    >
+      <Text style={styles.buttonText}>Start Searching!</Text>
+    </Button>
+    </View>
+</ImageBackground>
     </View>
     </NativeBaseProvider>
   );
@@ -18,6 +27,24 @@ export default function App() {
 const styles = {
   container: {
     flex: 1,
-    marginTop: Platform.OS === "android" ? 24 : 0
+    marginTop: Platform.OS === "android" ? 64 : 0,
+  },
+  viewStyle:{
+    flex:1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleStyle:{
+    fontSize: 30,
+    color: 'blue',
+    alignItems: 'center'
+  },
+  buttonStyle:{
+    margin:10
+  },
+  buttonText:{
+    color:'white'
   }
+
 }
