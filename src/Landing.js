7d47fe5160319d590129b98778d87eb37c266172
Prasanth Louis <prasanthlouis21@gmail.com>
2023-04-28 22:09:45 -0400

    Run Prettier

--- src/Landing.js
+++ src/Landing.js
@@ -1,51 +1,48 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
-import { Button } from 'native-base'
-let myBackground = require('../assets/landing.jpg');
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import { Button } from "native-base";
+let myBackground = require("../assets/landing.jpg");
 class Landing extends React.Component {
-    render() {
-        return (
-            <View>
-                <ImageBackground source={myBackground} style={{width: '100%', height: '100%'}}>
-  <View style={styles.viewStyle}>
-    <Text style={styles.titleStyle}> Welcome to PokeSearch</Text>
-    <Button
-    block={true}
-    style={styles.buttonStyle}
-    onPress={() => {}}
-    >
-      <Text style={styles.buttonText}>Start Searching!</Text>
-    </Button>
-    </View>
-</ImageBackground>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <ImageBackground
+          source={myBackground}
+          style={{ width: "100%", height: "100%" }}>
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}> Welcome to PokeSearch</Text>
+            <Button block={true} style={styles.buttonStyle} onPress={() => {}}>
+              <Text style={styles.buttonText}>Start Searching!</Text>
+            </Button>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = {
-    container: {
-      flex: 1,
-      marginTop: Platform.OS === "android" ? 64 : 0,
-    },
-    viewStyle:{
-      flex:1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    titleStyle:{
-      fontSize: 30,
-      color: 'blue',
-      alignItems: 'center'
-    },
-    buttonStyle:{
-      margin:10
-    },
-    buttonText:{
-      color:'white'
-    }
-  
-  }
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === "android" ? 64 : 0,
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: "blue",
+    alignItems: "center",
+  },
+  buttonStyle: {
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+};
 
 export default Landing;
