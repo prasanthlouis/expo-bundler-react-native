ae7ad05c70902402e833dfe92bf81bf9d44c917f
Prasanth Louis <prasanthlouis21@gmail.com>
2023-04-28 21:47:00 -0400

    Quick component using native base

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Platform } from 'react-native';
+import { Button, NativeBaseProvider } from 'native-base';
+
 
 export default function App() {
   return (
+    <NativeBaseProvider>
     <View style={styles.container}>
       <Text>Open up App.js tosds start working on Hello's app!</Text>
+      <Button><Text>Hello World</Text></Button>
       <StatusBar style="auto" />
     </View>
+    </NativeBaseProvider>
   );
 }
 
