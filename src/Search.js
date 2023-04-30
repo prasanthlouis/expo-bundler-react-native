7d9e769c24f1c21ba0875ac1a184535997a2c082
Prasanth Louis <prasanthlouis21@gmail.com>
2023-04-30 13:32:58 -0400

    Editing search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import {
+  HStack,
+  Item,
+  Icon,
+  Input,
+  IconButton,
+  MaterialIcons,
+} from "native-base";
 class Search extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Search Page</Text>
+      <View style={{ flex: 1 }}>
+        <HStack bg="violet.800" alignItems="center">
+          <HStack alignItems="center">
+            <Text>Pokemon Search</Text>
+          </HStack>
+        </HStack>
       </View>
     );
   }
