0d5f8348658b2ebb3a45555bcdd6227a27c15861
Prasanth Louis <prasanthlouis21@gmail.com>
2023-04-30 14:33:05 -0400

    Adding API

--- src/Search.js
+++ src/Search.js
@@ -4,12 +4,29 @@ import { HStack, Icon, Input, StatusBar, Box, Item } from "native-base";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import PokeLoader from "./PokeLoader";
 import SearchBody from "./SearchBody";
+import axios from "axios";
 class Search extends React.Component {
   state = {
     pokeSearch: "",
     onCall: true,
   };
-  searchPoke = () => {};
+  searchPoke = () => {
+    this.setState({ onCall: true });
+    var self = this;
+    axios
+      .get(
+        "http://pokeapi.co/api/v2/pokemon/" +
+          this.state.pokeSearch.toLowerCase()
+      )
+      .then(function (response) {
+        console.log(response.data);
+        self.setState({ data: response.data });
+        self.setState({ onCall: false });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   renderBody = () => {
     if (this.state.onCall) {
       return <PokeLoader />;
@@ -44,6 +61,7 @@ class Search extends React.Component {
                   size="4"
                   color="gray.400"
                   as={<Ionicons name="ios-search" />}
+                  onPress={this.searchPoke}
                 />
               }
             />
