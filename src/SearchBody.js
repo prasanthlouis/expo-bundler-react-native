9e00ee20ca557a705cfd6172d9be6da193210dad
Prasanth Louis <prasanthlouis21@gmail.com>
2023-05-01 22:01:39 -0400

    Adding eggshell background plus size stats

--- src/SearchBody.js
+++ src/SearchBody.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text, ScrollView, Image } from "react-native";
+import { FlatList, Box, Divider } from "native-base";
 
 class SearchBody extends React.Component {
   render() {
     const pokemon = this.props.data;
     console.log(pokemon.name);
+    const data = [
+      {
+        weight: pokemon.weight,
+        height: pokemon.height,
+      },
+    ];
 
     return (
       <View>
@@ -15,6 +22,30 @@ class SearchBody extends React.Component {
           <Image
             source={{ uri: pokemon.sprites.front_default }}
             style={styles.img}
+          />
+        </View>
+        <View style={styles.info}>
+          {/*           <ListItem>
+            <Text style={{ fontWeight: "bold" }}>Size</Text>
+          </ListItem> */}
+          {/*           <ListItem>
+            <Text style={{ fontWeight: "bold" }}>
+              Height = {pokemon.height}
+            </Text>
+          </ListItem> */}
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <View>
+                <Box px={2} py={2} rounded="md">
+                  <Text style={{ fontWeight: "bold" }}>Size</Text>
+                  <Divider bg="AD6" thickness="6" orientation="horizontal" />
+                  <Text>Weight - {item.weight}</Text>
+                  <Divider bg="AD6" thickness="2" orientation="horizontal" />
+                  <Text>Height - {item.height}</Text>
+                </Box>
+              </View>
+            )}
           />
         </View>
       </View>
@@ -37,6 +68,11 @@ const styles = {
     width: 200,
     justifyContent: "center",
     alignItems: "center",
+  },
+  info: {
+    backgroundColor: "#F0EAD6",
+    opacity: 0.8,
+    padding: 10,
   },
 };
 
